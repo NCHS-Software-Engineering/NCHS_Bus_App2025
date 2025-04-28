733cfaebb3e2be3cffb942af1b05c4d7e91e7b77
pagrawal-cs <pagrawal@stu.naperville203.org>
2025-04-28 13:39:39 -0500

    Trying another way to fix starredBusses problem

--- server.js
+++ server.js
@@ -304,7 +304,7 @@ async function sendNotification(data) {
   if (isIOSUser(data)) {
     sendNotificationToiOS("Bus Update", `Bus #${data.number} has ${data.newStatus}`);
   } else {
-    const subscriptions = await Subscription.find({ starred: { $in: [Number(data.number)] } });
+    const subscriptions = await Subscription.find({ starred: { $in: [String(data.number)] } });
 
    // const subscriptions = await Subscription.find({ starred: { $in: [data.number] } }); // Gets all subs that have this bus starred
     console.log(data.number);
