5bfdb8886efed1e15e176275206667a0f7edbfe8
pagrawal-cs <pagrawal@stu.naperville203.org>
2025-04-29 13:43:10 -0500

    Undid the change

--- server.js
+++ server.js
@@ -304,9 +304,9 @@ async function sendNotification(data) {
   if (isIOSUser(data)) {
     sendNotificationToiOS("Bus Update", `Bus #${data.number} has ${data.newStatus}`);
   } else {
-    //const subscriptions = await Subscription.find({ starred: { $in: [data.number] } }); // Gets all subs that have this bus starred
+    const subscriptions = await Subscription.find({ starred: { $in: [data.number] } }); // Gets all subs that have this bus starred
 
-    const subscriptions = await Subscription.find({ starred: { $in: [data.number.toString()] } }); // Gets all subs that have this bus starred
+    //const subscriptions = await Subscription.find({ starred: { $in: [data.number.toString()] } }); // Gets all subs that have this bus starred
 
     console.log(data.number);
     console.log(data.newStatus);
